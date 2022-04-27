c833fff3e4d204f7fb4175033beef7d035b75c90
Adrian Siekierka <kontakt@asie.pl>
2022-04-27 21:45:16 +0200

    fix input code

--- src/input.c
+++ src/input.c
@@ -1,14 +1,16 @@
 #include <gb/gb.h>
 #include "input.h"
 
-#define JOY_REPEAT_DELAY 15
+//#define JOY_REPEAT_DELAY 15
+#define JOY_REPEAT_DELAY 11
 #define JOY_REPEAT_DELAY_NEXT 2
 
 extern uint8_t vbl_ticks;
 
 static uint8_t input_keys = 0;
+static uint8_t input_keys_repressed = 0;
 static uint8_t input_keys_released = 0;
-uint8_t input_pressed;
+uint8_t input_held;
 static uint8_t input_vbls_next[4];
 static uint8_t input_last = 0;
 
@@ -17,35 +19,47 @@ int8_t input_delta_x, input_delta_y;
 void input_update_vbl(void) {
 	uint8_t keys = joypad();
 	input_keys |= keys;
-	input_keys_released |= (input_pressed & (~keys));
+	input_keys_repressed |= (keys & input_keys_released);
+	input_keys_released |= (input_held & (~keys));
 }
 
 void input_update(void) {
-	uint8_t keys;
+	uint8_t keys_pressed;
+	uint8_t keys_repressed;
+	uint8_t keys_released;
+
 	CRITICAL {
-		keys = input_keys;
-		input_keys = 0;
-		input_pressed &= ~input_keys_released;
-		input_keys_released = 0;
+		keys_pressed = input_keys;
+		keys_repressed = input_keys_repressed;
+		keys_released = input_keys_released;
 	}
 
 	input_delta_x = 0;
 	input_delta_y = 0;
 
-	input_pressed &= 0x0F;
-	input_pressed |= (keys & 0xF0);
-	if (keys & 0x0F) for (uint8_t i = 0; i < 4; i++) {
+	input_held = (input_held & 0x0F) | (keys_pressed & 0xF0);
+	if (keys_pressed & 0x0F) for (uint8_t i = 0; i < 4; i++) {
 		uint8_t input_id = (input_last + 1) & 3;
 		uint8_t input_mask = 1 << input_id;
 		input_last = input_id;
 
-		if (keys & input_mask) {
-			if (input_pressed & input_mask) {
+		if (keys_pressed & input_mask) {
+			if (keys_repressed & input_mask) {
+				goto KeyRepressed;
+			} else if (input_held & input_mask) {
+				if (keys_released & input_mask) {
+					input_held &= ~input_mask;
+				}
 				if (((uint8_t) (input_vbls_next[input_id] - vbl_ticks)) < 0x80) continue;
-				input_vbls_next[input_id] = vbl_ticks + JOY_REPEAT_DELAY_NEXT;
+				if (!(keys_released & input_mask)) {
+					input_vbls_next[input_id] = vbl_ticks + JOY_REPEAT_DELAY_NEXT;
+				}
 			} else {
-				input_pressed |= input_mask;
-				input_vbls_next[input_id] = vbl_ticks + JOY_REPEAT_DELAY;
+				if (!(keys_released & input_mask)) {
+KeyRepressed:
+					input_held |= input_mask;
+					input_vbls_next[input_id] = vbl_ticks + JOY_REPEAT_DELAY;
+				}
 			}
 			if (input_mask == J_UP) {
 				input_delta_y = -1;
@@ -58,5 +72,12 @@ void input_update(void) {
 			}
 			break;
 		}
+	}
+
+	CRITICAL {
+		input_keys = 0;
+		input_keys_repressed = 0;
+		input_keys_released = 0;
+		input_update_vbl();
 	}
 }
--- src/input.h
+++ src/input.h
@@ -6,12 +6,12 @@
 #include <gb/gb.h>
 
 extern int8_t input_delta_x, input_delta_y;
-extern uint8_t input_pressed;
+extern uint8_t input_held;
 
-#define input_a_pressed ((input_pressed & J_A) != 0)
-#define input_b_pressed ((input_pressed & J_B) != 0)
-#define input_select_pressed ((input_pressed & J_SELECT) != 0)
-#define input_start_pressed ((input_pressed & J_START) != 0)
+#define input_a_pressed ((input_held & J_A) != 0)
+#define input_b_pressed ((input_held & J_B) != 0)
+#define input_select_pressed ((input_held & J_SELECT) != 0)
+#define input_start_pressed ((input_held & J_START) != 0)
 #define input_shift_pressed input_a_pressed
 
 void input_update(void);
