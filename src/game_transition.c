a4c3ecf436d64a154738c9dd94d42a6b26ef5640
Adrian Siekierka <kontakt@asie.pl>
2022-04-29 20:32:09 +0200

    save ~900 bytes of bank 0

--- src/game_transition.c
+++ src/game_transition.c
@@ -1,12 +1,14 @@
 #include "game.h"
 
 #include <gb/gb.h>
+#include "bank_switch.h"
 #include "config.h"
 #include "game_transition.h"
 
 #ifdef FEAT_BOARD_TRANSITIONS
 
-#include "../res/game_transition_table.inc"
+#define TRANSITION_TABLE_20_17_ENTRY_COUNT 340
+extern const uint8_t transition_table_20_17[];
 
 // must be a divisor of the table size
 
@@ -29,8 +31,9 @@ __endasm;
 void game_transition_step(void) {
 	if (transition_pos >= (TRANSITION_TABLE_20_17_ENTRY_COUNT * 2)) return;
 
-	uint8_t len;
 	const uint8_t *arr = transition_table_20_17 + transition_pos;
+	uint8_t prev_bank = _current_bank;
+	ZOO_SWITCH_ROM(3);
 
 	if (transition_color == 0xFF) {
 		{
@@ -63,50 +66,17 @@ void game_transition_step(void) {
 			uint8_t y = *(arr++);
 			board_draw_tile(viewport_x + x, viewport_y + y);
 		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			board_draw_tile(viewport_x + x, viewport_y + y);
-		}
-		transition_pos += 14;
+		transition_pos += 12;
 	} else {
-		{
+		for (uint8_t i = 0; i < 7; i++) {
 			uint8_t x = *(arr++);
 			uint8_t y = *(arr++);
 			text_draw(x, y, 178, transition_color);
 		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			text_draw(x, y, 178, transition_color);
-		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			text_draw(x, y, 178, transition_color);
-		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			text_draw(x, y, 178, transition_color);
-		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			text_draw(x, y, 178, transition_color);
-		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			text_draw(x, y, 178, transition_color);
-		}
-		{
-			uint8_t x = *(arr++);
-			uint8_t y = *(arr++);
-			text_draw(x, y, 178, transition_color);
-		}
-		transition_pos += 14;
+		transition_pos += 16;
 	}
+
+	ZOO_SWITCH_ROM(prev_bank);
 }
 
 bool game_transition_running(void) {
--- tools/gen_transition_table.py
+++ tools/gen_transition_table.py
@@ -6,6 +6,8 @@ if len(sys.argv) > 4:
 	r_seed = int(sys.argv[4])
 random.seed(r_seed)
 
+t_padding = 4
+
 with open(sys.argv[1], "w") as fp:
 	width = int(sys.argv[2])
 	height = int(sys.argv[3])
@@ -13,7 +15,7 @@ with open(sys.argv[1], "w") as fp:
 
 	print("// Auto-generated file. Please do not edit directly.\n", file = fp)
 	print("#define TRANSITION_TABLE_%d_%d_ENTRY_COUNT %d" % (width, height, entry_count), file = fp)
-	print("static const uint8_t transition_table_%d_%d[] = {" % (width, height), file = fp)
+	print("const uint8_t transition_table_%d_%d[] = {" % (width, height), file = fp)
 
 	entries = []
 	for i in range(0, entry_count):
@@ -21,6 +23,9 @@ with open(sys.argv[1], "w") as fp:
 	random.shuffle(entries)
 
 	for i in entries:
+		print("\t%d, %d," % ((i % width), int(i / width)), file = fp)
+	for j in range(0, t_padding):
+		i = random.choice(entries)
 		print("\t%d, %d," % ((i % width), int(i / width)), file = fp)
 
 	print("};", file = fp);
