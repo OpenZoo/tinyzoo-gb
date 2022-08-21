ded139e020e474affd9516c9b23c9b3769e35626
Adrian Siekierka <kontakt@asie.pl>
2022-08-21 10:22:13 +0200

    sound: hacky drums implementation

--- src/game_sidebar.c
+++ src/game_sidebar.c
@@ -7,42 +7,56 @@
 #include "renderer_sidebar.h"
 
 void game_update_sidebar_health(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	if (zoo_world_info.health < 0) {
 		zoo_world_info.health = 0;
 	}
 
 	sidebar_draw_panel(0 >> 1, 2, 3, zoo_world_info.health, false);
+#endif
 }
 
 void game_update_sidebar_ammo(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	sidebar_draw_panel(6 >> 1, 132, 3, zoo_world_info.ammo, false);
+#endif
 }
 
 void game_update_sidebar_gems_time(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	if (zoo_board_info.time_limit_sec > 0) {
 		sidebar_draw_panel(12 >> 1, 'T', 3, zoo_board_info.time_limit_sec - zoo_world_info.board_time_sec, false);
 	} else {
 		sidebar_draw_panel(12 >> 1, 4, 2, zoo_world_info.gems, false);
 	}
+#endif
 }
 
 void game_update_sidebar_torches(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	sidebar_draw_panel(18 >> 1, 157, 1, zoo_world_info.torches, false);
+#endif
 }
 
 void game_update_sidebar_score(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	sidebar_draw_panel(24 >> 1, '+', 3, zoo_world_info.score, true);
+#endif
 }
 
 void game_update_sidebar_keys(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	sidebar_draw_keys(32 >> 1, zoo_world_info.keys);
+#endif
 }
 
 void game_update_sidebar_all(void) {
+#ifndef HACK_HIDE_STATUSBAR
 	game_update_sidebar_health();
 	game_update_sidebar_ammo();
 	game_update_sidebar_gems_time();
 	game_update_sidebar_torches();
 	game_update_sidebar_score();
 	game_update_sidebar_keys();
+#endif
 }
--- src/platform_gb/sound.c
+++ src/platform_gb/sound.c
@@ -34,6 +34,18 @@ __asm
 	; _sound_duration_counter < 0
 	bit 7, a
 	jr z, .timer_handler_sound_end
+	jr .timer_handler_next_note
+
+.timer_handler_no_sound:
+	xor a, a
+	ld (_sound_is_playing), a
+	ldh (_NR21_REG + 0), a
+	ldh (_NR30_REG + 0), a
+	ldh (_NR52_REG + 0), a
+
+.timer_handler_sound_end:
+	pop		bc
+	ret
 
 .timer_handler_next_note:
 	ld a, (_sound_buffer_len)
@@ -45,8 +57,9 @@ __asm
 	; init sound
 	ld a, #0x80
 	ldh (_NR52_REG + 0), a
-	xor a, a
+	ld a, #0x66
 	ldh (_NR51_REG + 0), a
+	xor a, a
 	ld b, a ; for sound_buffer_pos
 	dec a
 	; ld a, #0xFF
@@ -70,8 +83,6 @@ __asm
 	; ld b, #0x00 - still set from above
 	ld hl, #(_sound_freqs)
 	add hl, bc
-	ld a, #0x80
-	ldh (_NR21_REG + 0), a
 	ld a, #0xF0
 	ldh (_NR22_REG + 0), a
 	ld a, (hl+)
@@ -80,12 +91,37 @@ __asm
 	bit 7, a
 	jr z, .timer_handler_checks_done ; dummy note
 	ldh (_NR24_REG + 0), a
+	ld a, #0x80
+	ldh (_NR21_REG + 0), a
 	ld a, #0x22
-	ldh (_NR51_REG + 0), a
+	; ldh (_NR51_REG + 0), a
 	jr .timer_handler_checks_done
 
 .timer_handler_check_drum:
-	; TODO: drum support
+	sub a, #224
+	jr c, .timer_handler_checks_done
+	cp a, #10
+	jr nc, .timer_handler_checks_done
+	add a, a
+	add a, a
+	ld c, a
+	; ld b, #0x00 - still set from above
+	ld hl, #(_sound_drums_data)
+	add hl, bc
+	ld a, (hl+)
+	ldh (_NR31_REG + 0), a
+	ld a, (hl+)
+	ldh (_NR32_REG + 0), a
+	ld a, (hl+)
+	ldh (_NR33_REG + 0), a
+	ld a, (hl)
+	bit 7, a
+	jr z, .timer_handler_checks_done ; dummy drum
+	ldh (_NR34_REG + 0), a
+	ld a, #0x80
+	ldh (_NR30_REG + 0), a
+	ld a, #0x44
+	; ldh (_NR51_REG + 0), a
 
 .timer_handler_checks_done:
 	pop hl
@@ -98,23 +134,28 @@ __asm
 	inc a
 	inc a
 	ld (_sound_buffer_pos), a
-	jr .timer_handler_sound_end
+	jp .timer_handler_sound_end
 
-.timer_handler_no_sound:
-	xor a, a
-	ld (_sound_is_playing), a
-	ldh (_NR52_REG + 0), a
-
-.timer_handler_sound_end:
-	pop		bc
 __endasm;
 }
+
+/* from hUGETracker - public domain */
+static uint8_t wave_ram_data[] = {
+	0x44, 0x1C, 0x5A, 0x43, 0xE3, 0x99, 0x76, 0xDD,
+	0x3C, 0x98, 0x98, 0xCE, 0x97, 0xBE, 0xDC, 0x2A
+};
 
 void sound_init(void) BANKED {
 	sound_enabled = true;
 	sound_block_queueing = false;
 	sound_buffer_len = 0;
 	sound_is_playing = false;
+
+	// configure wave RAM
+	volatile uint8_t *wptr = (volatile uint8_t*) 0xFF30;
+	for (uint8_t i = 0; i < 16; i++, wptr++) {
+		*wptr = wave_ram_data[i];
+	}
 }
 
 void sound_clear_queue(void) BANKED {
