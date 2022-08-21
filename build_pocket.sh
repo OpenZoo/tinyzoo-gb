7a594a521a1cedbd35d540511a6832c3b3660940
Adrian Siekierka <kontakt@asie.pl>
2022-08-21 12:41:21 +0200

    pocket: fix build

--- build_pocket.sh
+++ build_pocket.sh
@@ -3,7 +3,7 @@ if [ -f obj/pocket/gbzoo.pocket ]; then
 	rm obj/pocket/gbzoo.pocket
 fi
 make -j7 pocket
-cat obj/pocket/gbzoo.pocket TOWN.BIN > gbzoo.pocket
-python3 tools/fix_rom_size.py gbzoo.pocket
-cp gbzoo.pocket obj/pocket/gbzoo.pocket
-rm gbzoo.pocket
+#cat obj/pocket/gbzoo.pocket TOWN.BIN > gbzoo.pocket
+#python3 tools/fix_rom_size.py gbzoo.pocket
+#cp gbzoo.pocket obj/pocket/gbzoo.pocket
+#rm gbzoo.pocket
