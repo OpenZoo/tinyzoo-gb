a6dd910d17d58b95da4e8c4b321e4f799eedc5f0
Adrian Siekierka <kontakt@asie.pl>
2023-04-02 21:25:20 +0200

    fix warnings on new SDCC versions

--- src/platform_gb/himem.h
+++ src/platform_gb/himem.h
@@ -46,16 +46,16 @@
 
 #include <gbdk/platform.h>
 
-static volatile SFR AT(0xA0) temp1; // used in VBLANK
-static volatile SFR AT(0xA1) temp2; // used in VBLANK
-static volatile SFR AT(0xA2) temp3; // used in VBLANK
-static volatile SFR AT(0xA3) temp4; // used in VBLANK
-static volatile SFR AT(0xA4) temp5; // used in VBLANK
-static volatile SFR AT(0xA5) ly_bank_switch_mirror;
-static volatile SFR AT(0xA6) zoo_stat_count;
-static volatile SFR AT(0xA7) temp6; // NOT used in VBLANK
-static volatile SFR AT(0xA8) ly_offset_mirror;
-static volatile SFR AT(0xA9) temp7; // NOT used in VBLANK
-static volatile SFR AT(0xAA) temp8; // NOT used in VBLANK
+static volatile SFR AT(0xFFA0) temp1; // used in VBLANK
+static volatile SFR AT(0xFFA1) temp2; // used in VBLANK
+static volatile SFR AT(0xFFA2) temp3; // used in VBLANK
+static volatile SFR AT(0xFFA3) temp4; // used in VBLANK
+static volatile SFR AT(0xFFA4) temp5; // used in VBLANK
+static volatile SFR AT(0xFFA5) ly_bank_switch_mirror;
+static volatile SFR AT(0xFFA6) zoo_stat_count;
+static volatile SFR AT(0xFFA7) temp6; // NOT used in VBLANK
+static volatile SFR AT(0xFFA8) ly_offset_mirror;
+static volatile SFR AT(0xFFA9) temp7; // NOT used in VBLANK
+static volatile SFR AT(0xFFAA) temp8; // NOT used in VBLANK
 
 #endif
